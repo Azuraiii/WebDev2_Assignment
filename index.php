6fef752c561b47004632a51c6cd32d3ad42e4296
Jelo Salomon <c14429822@mydit.ie>
2016-10-30 16:12:57 +0000

    Added index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE_HTML>
+<html>
+    <head>
+        <?php
+        /**
+         * Created by PhpStorm.
+         * User: azkei
+         * Date: 30/10/2016
+         * Time: 16:02
+         */
+
+        ?>
+
+        <link rel="stylesheet" href="styling/styles.css">
+        <title> Home Page</title>
+
+    </head>
+        <div id="menu">
+            <ul>
+                <li class ="Home"> <a href="index.php"> Home</a> </li>
+                <li><a href=""></a> </li>
+            </ul>
+        </div>
+    <body>
+
+    </body>
+</html>
+
